6830895cda730511269bf35bb580a97677477fa4
jaejae <buskingestagro@gmail.com>
2022-06-06 23:22:46 +0900

    6장 시작

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import Button from "./Button";
-import stylesApp from "./App.module.css";
+import {useState, useEffect} from "react";
 
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onChange = (e) => 
+  {
+    setKeyword(e.target.value);
+  }
+  const onClick = () => 
+  {
+    setValue((prev) => prev +1);
+  }
+  console.log('run all times');  
+  useEffect( () =>
+  {
+    console.log('i run only once');
+  }, []);  
+  useEffect( () =>
+  {    
+    console.log("i run when keyword changes");        
+  }, [keyword]);  
+
+  useEffect( () =>
+  {
+    console.log('i run when counter changes');
+  }, [counter]);  
+
   return (
     <div className="App">
-      <h1 className={stylesApp.title}>Welcome back</h1>
-      <Button text={"Continue"}/>
+      <input 
+        onChange ={onChange} 
+        value = {keyword}
+        type ="text" 
+        placeholder="Search here..."
+      />
+      <h1> {counter} </h1>
+      <button onClick ={onClick}>clickMe</button> 
     </div>
   );
 }
