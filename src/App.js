4165f7ca8c6eb3f0fe7ccc37a8f58c08ef441d97
buskingestagro@gmail.com <buskingestagro@gmail.com>
2022-06-17 18:35:43 +0900

    7.0 start

--- src/App.js
+++ src/App.js
@@ -1,42 +1,20 @@
 import {useState, useEffect} from "react";
 
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onChange = (e) => 
-  {
-    setKeyword(e.target.value);
-  }
-  const onClick = () => 
-  {
-    setValue((prev) => prev +1);
-  }
-  console.log('run all times');  
-  useEffect( () =>
-  {
-    console.log('i run only once');
-  }, []);  
-  useEffect( () =>
-  {    
-    console.log("i run when keyword changes");        
-  }, [keyword]);  
 
-  useEffect( () =>
-  {
-    console.log('i run when counter changes');
-  }, [counter]);  
+function App() {
+
+  const [toDo, setToDo] = useState("");
+  const onChange = (event) => setToDo(event.target.value);
+  console.log(toDo);
 
   return (
-    <div className="App">
-      <input 
-        onChange ={onChange} 
-        value = {keyword}
-        type ="text" 
-        placeholder="Search here..."
-      />
-      <h1> {counter} </h1>
-      <button onClick ={onClick}>clickMe</button> 
+    <div>
+      <input onChange={onChange} 
+      value = {toDo} 
+      type = "text" 
+      placeholder="Write your to do..." />
+          
     </div>
   );
 }
